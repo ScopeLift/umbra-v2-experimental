a252d01c0a99ae9b1cbff34e7e0b589d00983515
marcomariscal <marco.a.mariscal@gmail.com>
2024-05-09 19:04:04 -0700

    feat: rainbow setup

---
+++ src/app/providers.tsx
@@ -0,0 +1,48 @@
+'use client';
+
+import '@rainbow-me/rainbowkit/styles.css';
+
+import {
+  RainbowKitProvider,
+  getDefaultWallets,
+  getDefaultConfig
+} from '@rainbow-me/rainbowkit';
+import {
+  arbitrum,
+  base,
+  mainnet,
+  optimism,
+  polygon,
+  sepolia
+} from 'wagmi/chains';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { WagmiProvider } from 'wagmi';
+
+const { wallets } = getDefaultWallets();
+
+const config = getDefaultConfig({
+  appName: 'RainbowKit demo',
+  projectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID || '',
+  wallets: [...wallets],
+  chains: [
+    mainnet,
+    polygon,
+    optimism,
+    arbitrum,
+    base,
+    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [sepolia] : [])
+  ],
+  ssr: true
+});
+
+const queryClient = new QueryClient();
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>{children}</RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+}
